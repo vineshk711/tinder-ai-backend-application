410ca89425900feb985606b7fea03b6e17210702
Vinesh Kumar <vinesh.kumar@birdeye.com>
2024-07-27 16:09:54 +0530

    Add profile to DB and fetch successfully

--- src/main/java/com/vinesh/tinder_ai_backend/TinderAiBackendApplication.java
+++ src/main/java/com/vinesh/tinder_ai_backend/TinderAiBackendApplication.java
@@ -1,13 +1,37 @@
 package com.vinesh.tinder_ai_backend;
 
+import com.vinesh.tinder_ai_backend.profiles.Gender;
+import com.vinesh.tinder_ai_backend.profiles.Profile;
+import com.vinesh.tinder_ai_backend.profiles.ProfileRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TinderAiBackendApplication {
+public class TinderAiBackendApplication  implements CommandLineRunner {
+
+	@Autowired
+	private ProfileRepository profileRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(TinderAiBackendApplication.class, args);
+	}
+
+	public void run(String... args) throws Exception {
+		Profile profile = new Profile(
+				"1",
+				"Vinesh",
+				"Kumar",
+				25,
+				"Indian",
+				Gender.MALE,
+				"Software Engineer",
+				"foo.jpg",
+				"INTP"
+		);
+		profileRepository.save(profile);
+		profileRepository.findAll().forEach(System.out::println);
 	}
 
 }
---
+++ src/main/java/com/vinesh/tinder_ai_backend/profiles/Gender.java
@@ -0,0 +1,7 @@
+package com.vinesh.tinder_ai_backend.profiles;
+
+public enum Gender {
+    MALE,
+    FEMALE,
+    NON_BINARY
+}
