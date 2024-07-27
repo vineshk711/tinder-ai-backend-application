ff16ab47027db55c6e6c31976efd7a27ef85671c
Vinesh Kumar <vinesh.kumar@birdeye.com>
2024-07-27 15:46:12 +0530

    Init Application

---
+++ src/main/java/com/vinesh/tinder_ai_backend/TinderAiBackendApplication.java
@@ -0,0 +1,13 @@
+package com.vinesh.tinder_ai_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinderAiBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TinderAiBackendApplication.class, args);
+	}
+
+}
